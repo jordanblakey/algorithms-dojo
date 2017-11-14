e2194efb289b82b7bd3e4c4366f11dc83886e5a1
Jordan Blakey <jordan@jordanblakey.com>
2017-11-14 01:58:49 -0600

    Create extract_middle_character.py

---
+++ extract_middle_character.py
@@ -0,0 +1,8 @@
+def get_middle(s):
+#Return the middle character of the word. If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters.
+
+    if len(s)%2 == 0:
+        return (s[(len(s)/2)-1:(len(s)/2)+1])
+    else:
+        return s[len(s)/2]
