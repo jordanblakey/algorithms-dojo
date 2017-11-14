8e5ca86df1021302a3795e132976a0ea45b8b3e0
Jordan Blakey <jordan@jordanblakey.com>
2017-11-14 00:09:18 -0600

    Create string_redacting.py

---
+++ string_redacting.py
@@ -0,0 +1,13 @@
+# A song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".
+# Return a string with just the words of the song.
+
+def song_decoder(song):
+    song = song.replace("WUB", " ")
+    while '  ' in song:
+        song = song.replace("  ", " ")
+    if song[0] == " ":
+        song = song[1:]
+    if song[-1] == " ":
+        song = song[:-1]
+    print(song)
+    return song
