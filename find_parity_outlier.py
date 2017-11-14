81e75d4c573494e6cdf38d7770772390bff76dc4
Jordan Blakey <jordan@jordanblakey.com>
2017-11-13 23:50:39 -0600

    Create find_parity_outlier.py

---
+++ find_parity_outlier.py
@@ -0,0 +1,18 @@
+'''
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+'''
+
+def find_outlier(integers):
+    even = []
+    odd = []
+    print(integers)
+
+    for i in integers:
+      if i % 2 == 0:
+          even.append(i)
+      else:
+          odd.append(i)
+    if len(even) == 1:
+        return even[0]
+    if len(odd) == 1:
+        return odd[0]
