d3f4d81167bec10ef465f1feef5a9e5aeb1a7218
Jordan Blakey <jordan@jordanblakey.com>
2017-11-19 17:34:38 -0600

    Create get_local_maxima.py

---
+++ get_local_maxima.py
@@ -0,0 +1,17 @@
+def pick_peaks(arr):
+    #  returns positions and values of local maxima in a list.
+    p = [{'pos': [], 'peaks': []}]
+    def cp():
+        p[0]['pos'].append(i)
+        p[0]['p'].append(x)
+    for (i, x) in enumerate(arr):
+        if not (i == 0 or i == len(arr) - 1):
+            if cmp(x, arr[i - 1]) > 0 and cmp(x, arr[i + 1]) >= 0 and len(set(arr[i:])) != 1:
+                if cmp(x, arr[i + 1]) == 0:
+                    for j in arr[i:]:
+                        if j > x: break
+                        if j < x:
+                            cp()
+                            break
+                else: cp()
+    return p[0]
