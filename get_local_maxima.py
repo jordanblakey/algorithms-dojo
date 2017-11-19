138130fcb07012d9e3833f71b4be4dd986d19614
Jordan Blakey <jordan@jordanblakey.com>
2017-11-19 17:36:44 -0600

    Update get_local_maxima.py

--- get_local_maxima.py
+++ get_local_maxima.py
@@ -1,9 +1,9 @@
-def pick_peaks(arr):
+def glm(arr):
     #  returns positions and values of local maxima in a list.
     p = [{'pos': [], 'peaks': []}]
     def cp():
         p[0]['pos'].append(i)
-        p[0]['p'].append(x)
+        p[0]['peaks'].append(x)
     for (i, x) in enumerate(arr):
         if not (i == 0 or i == len(arr) - 1):
             if cmp(x, arr[i - 1]) > 0 and cmp(x, arr[i + 1]) >= 0 and len(set(arr[i:])) != 1:
