a5436c58ada817782025723959d1bf71a59cf9d8
Jordan Blakey <jordan@jordanblakey.com>
2017-11-25 08:11:45 -0600

    Create get_negative.java

---
+++ get_negative.java
@@ -0,0 +1,13 @@
+public class Kata {
+  public static int makeNegative(final int x) {
+    // Given an integer, return the negative of its absolute value.
+    int n = x;
+    if (x < 0) {
+    } else if (x > 0){
+      n = -n;
+    }
+    else {
+    }    
+    return n;
+  }
+}
