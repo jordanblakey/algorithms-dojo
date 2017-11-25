af1ba9b55372db7fd6c772fd9e62b72d54e97c88
Jordan Blakey <jordan@jordanblakey.com>
2017-11-25 14:41:57 -0600

    Create basice_math_functions.py

---
+++ Python/basice_math_functions.py
@@ -0,0 +1,7 @@
+# Create functions to represent all basic math operations on 2 arguments in sequence.
+def add(a,b): return a + b
+def multiply(a,b): return a * b
+def divide(a,b): return a / b
+def mod(a,b): return a % b
+def exponent(a,b): return a ** b
+def subt(a,b): return a - b
