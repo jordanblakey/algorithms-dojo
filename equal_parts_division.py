32ca1d364e71bb16400249d760c20645a81218d7
Jordan Blakey <jordan@jordanblakey.com>
2017-11-25 00:50:35 -0600

    Update equal_parts_division.py

--- equal_parts_division.py
+++ equal_parts_division.py
@@ -4,4 +4,8 @@ def maxlen(L1,L2):
     lim2 = max([L1, L2])/2
     if max([L1, L2])/3 > lim1: return max([L1, L2])/3
     return min([lim1, lim2])
-    
+
+# Cleaner
+def maxlen2(s1, s2):
+    sm, lg = sorted((s1, s2))
+    return min(max(lg / 3, sm), lg / 2)
