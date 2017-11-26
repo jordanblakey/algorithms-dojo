7f350f16da73b3121fe1ce40de06eda65eae4d6d
Jordan Blakey <jordan@jordanblakey.com>
2017-11-26 00:09:11 -0600

    Create cubes_volume_diff.py

---
+++ Python/cubes_volume_diff.py
@@ -0,0 +1,25 @@
+def find_difference(a, b):
+    # Given two lists representing the dimensions of cuboids, return the difference in their volumes.
+    def vol(l):
+      v = 1
+      for x in l: v *= x
+      return v
+    return abs(vol(a) - vol(b))
+
+
+# using operator.mul
+from operator import mul
+def find_difference2(a, b):
+    def vol(l): return reduce(mul, l)
+    return abs(vol(a) - vol(b))
+
+
+# Using reduce & a lambda function
+def find_difference3(a, b):      
+    def p(l): return reduce(lambda x,y: x*y, l)
+    return abs(p(a)-p(b))
+
+
+# Using numpy.product
+from numpy import product as p
+def vd(a,b): return abs(p(a)-p(b))
