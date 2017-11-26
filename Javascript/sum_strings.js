7bcece64265d5ad4bc7b8484b70741cafd106726
Jordan Blakey <jordan@jordanblakey.com>
2017-11-25 22:50:05 -0600

    Create sum_strings.js

---
+++ Javascript/sum_strings.js
@@ -0,0 +1,6 @@
+function sumStr(a,b) {
+  // Given 2 numeric strings, return their sum as a string.
+  let ai = parseInt(a), bi = parseInt(b);
+  let nan = x => !isNaN(x) ? x : null;
+  return (nan(ai) + nan(bi)).toString()
+}
