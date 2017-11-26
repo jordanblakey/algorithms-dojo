e691c5746d42a9153914034a95f63a116b15cc2b
Jordan Blakey <jordan@jordanblakey.com>
2017-11-26 00:13:10 -0600

    Update cubes_volume_diff.py

--- Python/cubes_volume_diff.py
+++ Python/cubes_volume_diff.py
@@ -7,19 +7,23 @@ def find_difference(a, b):
     return abs(vol(a) - vol(b))
 
 
-# using operator.mul
+# Using operator.mul:
 from operator import mul
 def find_difference2(a, b):
     def vol(l): return reduce(mul, l)
     return abs(vol(a) - vol(b))
 
 
-# Using reduce & a lambda function
+# Using reduce & a lambda function:
 def find_difference3(a, b):      
     def p(l): return reduce(lambda x,y: x*y, l)
     return abs(p(a)-p(b))
 
 
-# Using numpy.product
+# Using numpy.product:
 from numpy import product as p
 def vd(a,b): return abs(p(a)-p(b))
+
+
+# Native Python one-liner:
+def find_difference(a, b): return abs((a[0]*a[1]*a[2])-b[0]*b[1]*b[2])
