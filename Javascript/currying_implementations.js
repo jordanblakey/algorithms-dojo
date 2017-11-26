a3a5d3a5f6d5033d00881d1cfd56db43bcd3ef1b
Jordan Blakey <jordan@jordanblakey.com>
2017-11-25 20:47:54 -0600

    Update currying_implementations.js

--- Javascript/currying_implementations.js
+++ Javascript/currying_implementations.js
@@ -25,3 +25,20 @@ dragon2 = _.curry(dragon2)
 let ezraDragon = dragon2('Ezra')
 let tinyDragon2 = ezraDragon('tiny')
 console.log('[LODASH]:', tinyDragon2('peas'))
+
+
+// _.curry + filter().
+let dragons = [
+	{ name: 'fluffykis', element: 'lightning' },
+	{ name: 'noomi', element: 'lightning' },
+	{ name: 'karo', element: 'fire' },
+	{ name: 'doomer', element: 'timewarp' }
+
+]
+
+let hasElement = _.curry((element, obj) => obj.element === element);
+
+let lightningDragons = dragons.filter(hasElement('lightning'));
+for (var x in lightningDragons) {
+	console.log('[NO CURRY]:', lightningDragons[x])
+}
