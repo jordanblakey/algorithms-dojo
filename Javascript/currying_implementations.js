55265ba014e731b1afa3ec4971c5f7a6f0bca8f4
Jordan Blakey <jordan@jordanblakey.com>
2017-11-25 20:33:44 -0600

    Create currying_implementations.js

---
+++ Javascript/currying_implementations.js
@@ -0,0 +1,27 @@
+// Currying in ES6
+let dragon = 
+  name =>
+    size =>
+      element =>
+        name + ' is a ' +
+        size + ' dragon that breathes ' +
+        element + '!'
+
+let fluffykinsDragon = dragon('fluffykins')
+let tinyDragon = fluffykinsDragon('tiny')
+console.log('[ES6]:', tinyDragon('lightning'))
+
+
+// Currying using lodash
+import _ from 'lodash'
+
+let dragon2 = (name, size, element) =>
+	name + ' is a ' +
+	size + ' dragon that breathes ' +
+	element + '!'
+
+dragon2 = _.curry(dragon2)
+
+let bobDragon = dragon2('Bob')
+let tinyDragon2 = bobDragon('tiny')
+console.log('[LODASH]:', tinyDragon2('peas'))
