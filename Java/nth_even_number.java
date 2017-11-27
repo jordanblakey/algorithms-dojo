a9f4cbc201d5d46e7ea9cea68e63e165ef2d345f
Jordan Blakey <jordan@jordanblakey.com>
2017-11-26 20:32:05 -0600

    update

---
+++ Java/nth_even_number.java
@@ -0,0 +1,7 @@
+public class Num {
+  public static int nthEven(int n) {
+  // Given a number n, return the nth even number counting up from 0.
+    System.out.print(n*2-2);
+    return n*2-2;
+  }
+}
