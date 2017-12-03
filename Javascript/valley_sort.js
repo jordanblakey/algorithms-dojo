d9d2f7ac456cc8753b9ff043da04ce469d94b35f
Jordan Blakey <jordan@jordanblakey.com>
2017-12-03 17:04:47 -0600

    Update valley_sort.js

--- Javascript/valley_sort.js
+++ Javascript/valley_sort.js
@@ -2,6 +2,6 @@ function makeValley(arr) {
     // Given an array, sort the list in descending order from each side, making a valley
     let a = [], b = []    
     arr.sort((x, y) => y - x)
-      .forEach((x, i) => i % 2==0 ? b.push(arr[i]) : a.unshift(arr[i]))    
+      .forEach((x, i) => i % 2==0 ? b.push(x) : a.unshift(x)    
     return b.concat(a)
 }
