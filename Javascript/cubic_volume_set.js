7e2872c29c210767186e89614eb9bcb14777e161
Jordan Blakey <jordan@jordanblakey.com>
2017-12-03 19:30:11 -0600

    Create cubic_volume_set.js

---
+++ Javascript/cubic_volume_set.js
@@ -0,0 +1,15 @@
+function findNb(m) {
+  // Given a total volume for a stack of cubes, { n^3, (n-1)^3 ... },
+  // return the number of cubes needed to match the volume.
+  // If the # cannot be expressed as an iteger, return -1
+  let cube = (x) => Math.pow(x, 3)  
+  let sum = 0, i = 1, log = ''
+  
+  while (sum < m) {
+    sum += cube(i)
+    if (sum == m){ return i }
+    i += 1
+  }
+
+  return (-1)
+}
