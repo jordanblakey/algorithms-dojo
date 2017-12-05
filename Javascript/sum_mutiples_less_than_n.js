751c55376b4c5859d4dada30919f29d6df279f19
Jordan Blakey <jordan@jordanblakey.com>
2017-12-05 02:53:03 -0600

    Update sum_mutiples_less_than_n.js

--- Javascript/sum_mutiples_less_than_n.js
+++ Javascript/sum_mutiples_less_than_n.js
@@ -1,4 +1,4 @@
-function solution(n){
+function sumMultiples(n){
   // Given n, return the sum of multiples of 3 , 5 < n
   x = 1, acc = []
   while (x < n) {
@@ -13,4 +13,15 @@ function solution(n){
   acc = Array.from(acc)
   if (acc.length == 0) {return 0}
   return acc.reduce((a, x) => a + x)
+}
+
+
+# If we leave off the array it becomes much cleaner:
+function sumMultiples2(n) {
+  x = 1, sum = 0
+  while (x < n) {
+    if (x % 3 == 0 || x % 5 == 0) {sum += x}
+    x++
+  }
+  return sum
 }
