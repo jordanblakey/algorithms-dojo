9d5f043608ffc7f3195ce903ddbad728d53ebb44
Jordan Blakey <jordan@jordanblakey.com>
2017-12-05 23:14:13 -0600

    Create extract_int_from_string.js

---
+++ Javascript/extract_int_from_string.js
@@ -0,0 +1,7 @@
+function getAgeFromString(inputString){
+  // Given a string containing a single number, return that number as an int
+  return inputString = inputString
+    .split(' ')
+    .map(x => parseInt(x))
+    .filter(Number)[0]
+}
