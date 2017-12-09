66e2d9614cf3b28536b1eb52771dce97446e2b49
Jordan Blakey <jordan@jordanblakey.com>
2017-12-09 04:09:10 -0600

    reorganization

--- Javascript/ip_checker.js
+++ Javascript/ip_checker.js
@@ -4,11 +4,15 @@ function isValidIP(str) {
   str = str.split('.')
   if (str.length != 4) {return false}
   str = str.map(x => parseInt(x))
-  if (str.reduce((acc, x) => acc + x) === 0) {return true}
   for (x in str) {
     if (isNaN(str[x])) { return false }
     else if (!(str[x] >= 0) || !(str[x] <= 255)) { return false }
     else if (str[x].length === 3 && str[x] < 100) { return false }
   }
   return true
+}
+
+// This is why we have Regular Expressions.
+function isValidIP2(str) {
+  return (/^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$/).test(str);
 }
